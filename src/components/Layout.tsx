1d33d1120245631aef8ac52b8c5df52c38b8b959
Alexander <indievid_vk@vk.com>
2026-05-15 19:01:14 +0300

    refactor: Remove page transition animations
    Page transition animations are removed to simplify the layout and improve performance. The `AnimatePresence` and `motion.div` components are no longer used, and the main content is now a simple `div` element.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -46,18 +46,11 @@ export default function Layout() {
 
       {/* Main Content */}
       <main className="flex-1 overflow-y-auto relative flex flex-col">
-        <AnimatePresence mode="wait">
-          <motion.div 
-            key={location.pathname}
-            initial={{ opacity: 0, x: -10 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: 10 }}
-            transition={{ duration: 0.15, ease: "easeOut" }}
+          <div 
             className={isHome ? "absolute inset-0" : "w-full p-4 pb-[100px]"}
           >
             <Outlet />
-          </motion.div>
-        </AnimatePresence>
+          </div>
       </main>
 
       {!isHome && (
